9d685cab38b630b169c3bfbf42b1f386c7f23755
Saksham Sharma <sakshamsharma6253@gmail.com>
2026-06-09 14:44:10 +0530

    Removed footer and added responsive text to h1

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def static_files(path):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(on ahost='0.0.0.0', port=port, debug=True)
